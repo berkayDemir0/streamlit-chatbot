e04d5a6aea92d2deb304c11da50578db51b78c67
mac <mac@mac-MacBook-Pro-2.local>
2025-03-03 01:32:30 +0300

    Initial commit of Streamlit app

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from dotenv import load_dotenv
+from langchain import requests
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+API_KEY = os.getenv('GEMINI_API_KEY')
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","sen bir yapay zeka uzmanısın. otomasyon konusunda danışmanlık vereceksin. chatbot yapabiliyorsun. danışmanlık ücretsiz. chatbot için 10bin istiyorsun. model eğitimi de yapabiliyorsun. verdiğin cevaplar 50 karakteri geçmesin"),
+        ("user","Question:{question}")
+    ]
+)
+
+st.title("deneme")
+input_text=st.text_input("sohbet et")
+# Modeli "gemini-pro" yerine "gemini-1.5-pro" olarak değiştirdik
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=API_KEY)
+
+
+output_parsers=StrOutputParser()
+chain=prompt|llm|output_parsers
+
+if input_text:
+  st.write(chain.invoke({"question":input_text}))
